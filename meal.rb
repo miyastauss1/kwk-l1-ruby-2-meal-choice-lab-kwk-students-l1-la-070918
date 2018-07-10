bd7e06f93f305128a7af049059edcd2bc17cd285
Miya Stauss <miyastauss1@gmail.com>
2018-07-10 18:41:19 +0000

    Done.

---
+++ meal.rb
@@ -0,0 +1,34 @@
+def snacks(food=cheetos)
+  "Anytime, is the right time for #{food}!"
+end
+
+def breakfast(bfast = "frosted flakes")
+  "Morning is the best time for #{bfast}"
+end
+
+def lunch(lun = "grilled cheese")
+  "Noon is the best time for #{lun}"
+end
+
+def dinner(din = "salmon")
+  "Dinner is the best time for #{din}"
+end 
+
+puts breakfast("cheerios")
+puts lunch("chicken")
+puts dinner("steak")
+
+
+
+puts breakfast
+puts lunch
+puts dinner
+
+
+
+
+
+
+
+
+
