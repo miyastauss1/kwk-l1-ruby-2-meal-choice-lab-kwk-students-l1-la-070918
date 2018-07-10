1928de015630a76bfa0e9f7bdff13b12ef1bda20
Miya Stauss <miyastauss1@gmail.com>
2018-07-10 18:22:18 +0000

    automatically backed up by learn

--- meal.rb
+++ meal.rb
@@ -5,3 +5,12 @@ def meal_choice(breakfast="frosted flakes", lunch="grilled cheese", dinner="salm
 end
 
 meal_choice("french toast")
+
+
+def meal_choice(breakfast="frosted flakes", lunch="grilled cheese", dinner="salmon")
+  puts "Morning is the best time for #{breakfast}" 
+  puts "Lunch is the best time for #{lunch}"
+  puts "Dinner is the best time for #{dinner}"
+end
+
+meal_choice("french toast")
